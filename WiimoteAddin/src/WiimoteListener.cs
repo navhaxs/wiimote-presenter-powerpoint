c75b9576f0e1e161c96436500fc1a877258c2a46
Jeremy Wong <navhaxs@hotmail.com>
2015-02-14 11:36:03 +1100

    Added missing files to git.

---
+++ WiimoteAddin/src/WiimoteListener.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using WiimoteLib;
+
+namespace WiimoteAddin
+{
+    class WiimoteListener : IDisposable
+    {
+        ButtonState state = new ButtonState();
+        PowerPointApi pptController = new PowerPointApi(WiimoteAddin.Connect.thisPowerPoint);
+
+        public WiimoteListener()
+        {
+            if (App.wiimoteManager.WiimoteCount > 0)
+            {
+                state.ButtonStateChanged += OnButtonStateChanged;
+                try
+                {
+                    App.wiimoteManager.wiimoteCollection.FindAllWiimotes();
+                    int i = 0;
+                    foreach (WiimoteLib.Wiimote wm in App.wiimoteManager.wiimoteCollection)
+                    {
+                        wm.Connect();
+                    
+                        wm.SetReportType(InputReport.Buttons, true);
+
+                        if (i == 0)
+                        {
+                            Debug.Print("Added WiimoteChanged event to Wiimote 1.");
+                            wm.WiimoteChanged += UpdateWiimoteChanged;
+                        }
+
+
+                        i++;
+                    }          
+                }
+            catch (WiimoteLib.WiimoteNotFoundException)
+            {
+                // ignore this case
+                Debug.Print("No Wiimotes were found at this time.");
+            }
+            catch (Exception exception)
+            {
+                Debug.Print(exception.Message);
+            }
+
+            }
+
+        }
+
+        private void UpdateWiimoteChanged(object sender, WiimoteChangedEventArgs e)
+        {
+            state.Update(e.WiimoteState);
+        }
+
+        void OnButtonStateChanged(object sender, ButtonStateChangedArgs e)
+        {
+            if (e.buttonDown)
+            {
+                if (e.button == Button.Right || e.button == Button.Down || e.button == Button.A)
+                {
+                    pptController.NextSlide();
+                }
+                if (e.button == Button.Left || e.button == Button.Up || e.button == Button.B)
+                {
+                    pptController.PrevSlide();
+                }
+            }
+            
+        }
+
+        public void Dispose()
+        {
+            foreach (WiimoteLib.Wiimote wm in App.wiimoteManager.wiimoteCollection)
+            {
+                wm.SetLEDs(false, false, false, false);
+                wm.WiimoteChanged -= UpdateWiimoteChanged;
+                wm.SetReportType(InputReport.Buttons, false);
+                wm.Disconnect(); // release wiimote
+            }
+        }
+
+    }
+}
